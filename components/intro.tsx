a39cf9e2fb5bcdb548bd963d1a9969238d439af8
Sajin Shrestha <sajinshrestha@Sajins-MacBook-Air.local>
2024-11-22 19:17:00 -0500

    [Feat]: Portfolio Design with Implementation

---
+++ components/intro.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import authorImage from '@/public/images/authors/sajin.jpeg'
+
+export default function Intro() {
+  return (
+    <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
+      <div className='mt-2 flex-1 md:mt-0'>
+        <h1 className='title no-underline'>Hey, I'm Sajin.</h1>
+        <p className='mt-3 font-light text-muted-foreground'>
+          I'm a software engineer based in Toronto, Canada. I'm passionate about
+          learning new technologies and sharing knowledge with others.
+        </p>
+      </div>
+      <div className='relative'>
+        <Image
+          className='flex-1 rounded-lg grayscale'
+          src={authorImage}
+          alt='Sajin Shrestha'
+          width={175}
+          height={175}
+          priority
+        />
+      </div>
+    </section>
+  )
+}
---
+++ lib/utils.ts
@@ -0,0 +1,32 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: string) {
+  return new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  })
+}
+
+export const formattedDate = (date = new Date()) => {
+  const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+  console.log("User's Time Zone:", userTimeZone)
+
+  // Display time in the user's time zone
+
+  const localizedTime = date.toLocaleString('en-US', {
+    timeZone: userTimeZone,
+    hour: '2-digit',
+    minute: '2-digit',
+    // second: '2-digit',
+    hour12: true
+  })
+
+  console.log("Time in User's Time Zone:", localizedTime)
+  return localizedTime
+}
