7b3d5c21cea7c4aadc398c307e50b378c514e020
Sajin Shrestha <sajinshrestha@Sajins-MacBook-Air.local>
2024-11-25 18:00:11 -0500

    [Added]: single colons relpalced with &apos; on intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,10 +5,11 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Hey, I'm Sajin.</h1>
+        <h1 className='title no-underline'>Hey, I&apos;m Sajin.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I'm a software engineer based in Toronto, Canada. I'm passionate about
-          learning new technologies and sharing knowledge with others.
+          I&apos;m a software engineer based in Toronto, Canada. I&apos;m
+          passionate about learning new technologies and sharing knowledge with
+          others.
         </p>
       </div>
       <div className='relative'>
